a3c2ec244889f6ffad35b088247ebc138f38933e
Frederik Elischberger <frederik_eli@web.de>
2015-12-12 21:23:43 +0100

    Add comments for methods updateState and updateSettings

--- bot/BotState.java
+++ bot/BotState.java
@@ -53,27 +53,39 @@ public class BotState {
 		this.round = 0;
 		this.players = new HashMap<String, Player>();
 	}
-	
+
+	/**
+     * Initially set all necessary variables of our objects
+     *
+	 * @param key Contains one of the following predefined keywords returned from the game engine.
+	 * @param value Actual value of the key.
+	 */
 	public void updateSettings(String key, String value) {
 		switch(key) {
+            // settings timebank t:	Maximum time in milliseconds that your bot can have in its time bank
 			case "timebank":
 				this.MAX_TIMEBANK = Integer.parseInt(value);
 				timebank = MAX_TIMEBANK;
 				break;
+            // settings time_per_move t: Time in milliseconds that is added to your bot's time bank each move
 			case "time_per_move":
 				this.TIME_PER_MOVE = Integer.parseInt(value);
 				break;
+            //settings player_names [b,...]: A list of all player names in this match, including your bot's name
 			case "player_names":
 				String[] playerNames = value.split(",");
 				for(int i=0; i<playerNames.length; i++)
 					players.put(playerNames[i], new Player(playerNames[i]));
 				break;
+            // settings your_bot b: The name of your bot for this match
 			case "your_bot":
-				this.myBot = players.get(value); 
+				this.myBot = players.get(value);
 				break;
+            // settings field_width i: The width of the field, i.e. number of row-cells
 			case "field_width":
 				this.FIELD_WIDTH = Integer.parseInt(value);
 				break;
+            // settings field_height i: The height of the field, i.e. number of column-cells
 			case "field_height":
 				this.FIELD_HEIGHT = Integer.parseInt(value);
 				break;
@@ -82,30 +94,46 @@ public class BotState {
 				break;
 		}
 	}
-	
-	public void updateState(String player, String key, String value) {
+
+    /**
+     * Frequent updates of our bot about current points, skips, combos etc.
+     *
+     * @param player Name {b} of a bot (or "game").
+     * @param key Contains one of the following predefined keywords returned from the game engine.
+     * @param value Actual value of the key.
+     */
+    public void updateState(String player, String key, String value) {
 		switch(key) {
+            // update game round i: The number of the current round
 			case "round":
 				this.round = Integer.parseInt(value);
 				break;
+            // update game this_piece_type s: The type of the piece that has just spawned on the field
 			case "this_piece_type":
 				this.currentShape = ShapeType.valueOf(value);
 				break;
+            // update game next_piece_type s: The type of the piece that will spawn the next round
 			case "next_piece_type":
 				this.nextShape = ShapeType.valueOf(value);
 				break;
+            // update b row_points i: The amount of row points the given player has scored so far
 			case "row_points":
 				this.players.get(player).setPoints(Integer.parseInt(value));
 				break;
+            // update b combo i: The height of the current combo for the given player
 			case "combo":
 				this.players.get(player).setCombo(Integer.parseInt(value));
 				break;
+            // update b skips i: The amount of skips the given player has available
 			case "skips":
 				this.players.get(player).setSkips(Integer.parseInt(value));
 				break;
+            // update b field [[c,...];...]: The complete playing field of the given player
 			case "field":
 				this.players.get(player).setField(new Field(this.FIELD_WIDTH, this.FIELD_HEIGHT, value));
 				break;
+            // update game this_piece_position i,i:	The starting position in the field for
+            //                                      the current piece (top left corner of the piece bounding box)
 			case "this_piece_position":
 				String[] split = value.split(",");
 				this.shapeLocation = new Point(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
