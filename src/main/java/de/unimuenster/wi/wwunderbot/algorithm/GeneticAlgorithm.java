be8a4032f4cadedfc07de84002a73c73c2d3b573
MarcoNiemann <marco.niemann@uni-muenster.de>
2016-02-16 23:51:32 +0100

    Add JavaDoc to global attributes of GeneticAlgorithm.java

--- src/main/java/de/unimuenster/wi/wwunderbot/algorithm/GeneticAlgorithm.java
+++ src/main/java/de/unimuenster/wi/wwunderbot/algorithm/GeneticAlgorithm.java
@@ -22,6 +22,13 @@ import java.util.concurrent.TimeoutException;
  * @author Matthias
  */
 public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
+
+  /**
+   * List of all allowed moves that can be used as a mutation. Basically it is the list of all moves available, however
+   * "skip" and "drop" have been eliminated.
+   * "skip" is only available after certain combo moves and thus would not always be applicable; "drop" is undesirable
+   * since it would just finish the round (and place the piece).
+   */
   public final static MoveType[] ALLOWED_MUTATIONS = {
       MoveType.DOWN,
       MoveType.LEFT,
@@ -29,25 +36,125 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
       MoveType.TURNLEFT,
       MoveType.TURNRIGHT
   };
+
+  /**
+   * Remaining time in milliseconds at which the genetic algorithm will stop working to return a result in time.
+   */
   public final static long BREAK_MS = 15;
+
+  /**
+   * Maximum allowed number of iterations. Experiments have shown that this number usually is not reached which leaves
+   * sufficient potential for special cases where more computations are possible.
+   */
   public final static int MAX_NUMBER_OF_ITERATIONS = 100000;
+
+  /**
+   * Number of individuals per generation that will be generated.
+   */
   public final static int NUMBER_OF_INDIVIDUALS = 30;
+
+  /**
+   * Number of parents that will be transferred from one generation to another.
+   */
   public final static int NUMBER_OF_SELECTION = 7;
+
+  /**
+   * Maximum allowed mutation rate (mutation rate itself is dynamic). Ensures that reasonable results are created.
+   */
   public final static double MAX_MUTATION_RATE = 0.4;
+
+  /**
+   * Maximum allowed add-mutation rate (likelihood that an additional move is added).
+   */
   public final static double MAX_MUTATION_ADD_RATE = 0.4;
+
+  /**
+   * Maximum allowed deletion-mutation rate (likelihood that a move is deleted).
+   */
   public final static double MAX_MUTATION_REMOVE_RATE = 0.1;
+
+  /**
+   * Percentage of the length of the initial solution (the one identified by the heuristic) that a genome should have
+   * before {@code mutateRemoveMoves} is executed.
+   * This ensures that chromosomes are not shortened far under their needed length.
+   * If for example the {@code initialIndividual} would have 10 moves, at least 8 moves would be allowed for the
+   * mentioned function to still apply its functionality (deleting a part of the chromosome).
+   */
   public final static double MUTATION_MIN_LENGTH_PERCENTAGE = 0.8;
+
+  /**
+   * Percentage of the length of the initial solution (the one identified by the heuristic) that a genome should have
+   * before {@code mutateAddMoves} is executed.
+   * This ensures that chromosomes are not extended far over their needed length.
+   * If for example the {@code initialIndividual} would have 10 moves, at most 20 moves would be allowed for the
+   * mentioned function to still apply its functionality (adding an additional part to the chromosome).
+   */
   public final static double MUTATION_MAX_LENGTH_PERCENTAGE = 2;
+
+  /**
+   * Random number generator used to randomize the selection as well as the mutation of individuals respectively
+   * genomes.
+   */
   private final Random random = new Random();
+
+  /**
+   * {@code List} of {@code MovesArrayIndividual}s that later on represent the parent population (each
+   * {@code MovesArrayIndividual} that represents one potential [parent] solution).
+   */
   private final List<MovesArrayIndividual> parents = new ArrayList<>();
+
+  /**
+   * {@code PriorityQueue} that is used to automatically sort the population of {@code MovesArrayIndividual}s based
+   * on the score they have reached. It is initialized in reverse order, to have the individual with the highest score
+   * on the first place of the queue.
+   */
   private final PriorityQueue<MovesArrayIndividual> population = new PriorityQueue<>(Comparator.reverseOrder());
+
+  /**
+   * The initial individual that is the solution obtained in the heuristic that preceeds the genetic algorithm.
+   */
   private final MovesArrayIndividual initialIndividual;
+
+  /**
+   * Starting index for each shape in the {@code initialIndividual}. The starting index is the index with the first
+   * "down" move. This reduces the number of unuseful solutions, as significant changes can only be reached once the
+   * shape is moved to the ground. This holds true as all rotations and placements on the x-axis have already been
+   * tested by the heuristic. The genetic algorithm should only try to fit the shape into gaps, the heuristic could
+   * not find.
+   */
   private final int[] mutationStartIndex;
+
+  /**
+   * {@code BotState} object that stores all relevant information about the game.
+   */
   private final BotState state;
+
+  /**
+   * Holds a {@code BotStateEvaluationFunction} object that enables the genetic algorithm to evaluate the current state
+   * of the bot. This means that the contained object will help to evaluate the current field state by the attributes
+   * that have been proposed by this algorithm (complete lines, aggregated height, bumpiness and holes).
+   */
   private final BotStateEvaluationFunction evaluationFunction;
+
+  /**
+   * {@code Array} holding the {@code Shape}s representing the current and the next shape in the game.
+   */
   private final Shape[] shapes;
+
+  /**
+   * {@code boolean} control parameter to detect changes to the {@code List} of parent individuals. If changes occurred
+   * the total weighting used for randomly selecting a parent for reproduction has to be updated.
+   */
   private boolean parentsChanged = true;
+
+  /**
+   * {@code double} Initial weighting used to use weighted random selection to find parents for reproduction.
+   */
   private double totalWeight = 0;
+
+  /**
+   * {@code int} representing the number of iterations, the genetic algorithm already ran.
+   */
   private int iterations = 0;
 
   public GeneticAlgorithm(final Moves[] initialMoves, final BotState state, final BotStateEvaluationFunction evaluationFunction) {
