586ecbe2e1373e9b57a7be4f8b7fd00f91ac68ff
MarcoNiemann <marco.niemann@uni-muenster.de>
2016-02-17 01:00:51 +0100

    Add JavaDoc to methods of GeneticAlgorithm.java

--- src/main/java/de/unimuenster/wi/wwunderbot/algorithm/GeneticAlgorithm.java
+++ src/main/java/de/unimuenster/wi/wwunderbot/algorithm/GeneticAlgorithm.java
@@ -157,11 +157,23 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
    */
   private int iterations = 0;
 
+  /**
+   * Constructor for the {@code GeneticAlgorithm}.
+   *
+   * @param initialMoves  {@code Array} of {@code Moves} being the solution obtained by the {@code HeuristicAlgorithm}.
+   * @param state {@code BotState} object that stores all relevant information about the game.
+   * @param evaluationFunction  {@code BotStateEvaluationFunction} object that enabling the evaluation of the current
+   *                                                              state of the bot.
+   */
   public GeneticAlgorithm(final Moves[] initialMoves, final BotState state, final BotStateEvaluationFunction evaluationFunction) {
     this.state = state;
     this.evaluationFunction = evaluationFunction;
+
+    //  Setup the initial individual based on the moves obtained from the heuristic.
     this.initialIndividual = new MovesArrayIndividual(initialMoves);
 
+    //  Setup the array of mutation start indices. For each of the (normally two) shapes represented by the initialMoves
+    //  array, the first "down" command is searched and marked as the mutation start index.
     mutationStartIndex = new int[initialMoves.length];
     loop:
     for (int m = 0; m < initialMoves.length; m++) {
@@ -172,6 +184,7 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
         }
     }
 
+    //  Setup the shapes array by obtaining the current and the next shape from the BotState object.
     this.shapes = new Shape[]{
         this.state.getCurrentShape(),
         this.state.getNextShape()
@@ -196,11 +209,21 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
 
     System.err.println(iterations);
 
+    //  Return the initial individual whenever no population exists (timeout); in all other cases return the first
+    //  element of the population (which will have the highest score).
     return (population.size() == 0)
         ? initialIndividual.object
         : population.peek().object;
   }
 
+  /**
+   * Method to initially populate the {@code parents} {@code List} which then forms the basis for filling up the
+   * {@code population}. The function adds the initial individual to both, the parents as well as the population
+   * DTOs, and then adds a number of mutated initial individuals (as many as to fill up the {@code parents}
+   * {@code List}.
+   *
+   * @throws TimeoutException
+   */
   protected void initPopulation() throws TimeoutException {
     // Add initial individual
     evaluate(initialIndividual);
@@ -219,6 +242,12 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
     }
   }
 
+  /**
+   * Method that fills up the {@code population} {@code PriorityQueue}. As long as it does not contain a sufficient
+   * number of individuals two parent individuals are selected and used to create an additional offspring.
+   *
+   * @throws TimeoutException
+   */
   protected void populateAndEvaluate() throws TimeoutException {
     while (population.size() < NUMBER_OF_INDIVIDUALS) {
       // Create new offspring
@@ -243,11 +272,19 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
   }
 
   /**
-   * @return
+   * Method to have a parent selection based on weighted randomness. This means that parents having a higher score
+   * are more likely to get drawn than parents with a smaller score. However a certain degree of randomness is
+   * maintained.
+   *
    * @see <a href="http://stackoverflow.com/a/6737362">Stack Overflow answer<a/>
+   *
+   * @return  {@code MovesArrayIndividual} being the randomly selected parent
    */
   protected MovesArrayIndividual selectParent() {
+    //  If the parents list changed during the last iteration, the weights have to be updated.
     if (parentsChanged) updateTotalWeight();
+
+    //  Weighed-randomly draw a parent object.
     double r = random.nextDouble() * totalWeight;
     for (MovesArrayIndividual parent : parents) {
       r -= parent.score;
@@ -256,6 +293,10 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
     return parents.get(0);
   }
 
+  /**
+   * Helper-method that updates the total weight of all parent objects used for weighted-random selection. Basically
+   * sums up all scores of all parent individuals.
+   */
   private void updateTotalWeight() {
     totalWeight = 0;
     for (MovesArrayIndividual individual : parents)
@@ -263,15 +304,34 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
     parentsChanged = false;
   }
 
+  /**
+   * Method to apply a crossover to two {@code MovesArrayIndividuals}. Splits both individuals into three parts and
+   * recombines two elements of each with one element of the other ([1-2-1] and [2-1-2]).
+   * Which of the two combinations will be used is decided by chance.
+   *
+   * @param parentOne {@code MovesArrayIndividual} representing the first parent used for crossover
+   * @param parentTwo {@code MovesArrayIndividual} representing the second parent used for crossover
+   * @return  {@code MovesArrayIndividual} representing the newly combined individual.
+   */
   protected MovesArrayIndividual crossover(final MovesArrayIndividual parentOne, final MovesArrayIndividual parentTwo) {
+    //  Determine the number of shapes.
     final int size = shapes.length;
+
+    //  Create a new Moves-array for the offspring that will be created.
     final Moves[] offspringMovesArray = new Moves[size];
 
+    //  Create a new move-sequence for each shape which are then merged into the new MovesArrayIndividual.
     for (int i = 0; i < size; i++) {
+      //  Obtain the Moves sequences of the two parent individuals.
       final Moves movesParentOne = parentOne.object[i];
       final Moves movesParentTwo = parentTwo.object[i];
+
+      //  Determine the minimum size of the two sequences.
       final int minSize = Math.min(movesParentOne.size(), movesParentTwo.size());
 
+      //  If size is smaller or equal than two - indicating the nearby end of the game - the crossover step is 
+      //  omitted since for two elements a split into three would not work.
+      //  In this case the higher ranked individual gets returned.
       if (minSize <= 2)
         if (parentOne.score < parentTwo.score) {
           offspringMovesArray[i] = movesParentTwo.clone();
@@ -281,11 +341,16 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
           continue;
         }
 
+      //  Initialize a new Moves object for a given shape that will be used for crossover.
       final Moves offspringMoves = new Moves(shapes[i]);
 
+      //  Based on the length of the two Moves sequences and a random variable determine the positions for the 
+      //  crossover split.
       final int limit1 = random.nextInt(minSize - 1);
       final int limit2 = limit1 + 1 + random.nextInt(minSize - limit1 - 1);
 
+      //  Depending on chance create the new offspring via crossover of either two parts of parent one with one
+      //  element from parent two or vice versa.
       if (random.nextBoolean()) {
         offspringMoves.addAll(movesParentOne.subList(0, limit1));
         offspringMoves.addAll(movesParentTwo.subList(limit1, limit2));
@@ -302,13 +367,26 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
     return new MovesArrayIndividual(offspringMovesArray);
   }
 
+  /**
+   * Method that invokes all three mutation operations on a given individual.
+   * 
+   * @param individual {@code MovesArrayIndividual} that is going to be mutated.
+   */
   protected void mutate(final MovesArrayIndividual individual) {
     mutateChangeMoves(individual);
     mutateAddMoves(individual);
     mutateRemoveMoves(individual);
   }
 
+  /**
+   * Method that randomly changes moves in the given move sequence of an {@code MovesArrayIndividual}.
+   * This is applied for both shapes in the given individual.
+   * Whether a given move will be changed or not is random with a chance not greater than 40%.
+   * 
+   * @param individual {@code MovesArrayIndividual} that is going to be mutated.
+   */
   private void mutateChangeMoves(final MovesArrayIndividual individual) {
+    //  Iterate over all shapes in MovesArrayIndividual.
     for (int m = 0; m < individual.object.length; m++) {
       Moves moves = individual.object[m];
       // mutate every move except the last one (DROP)
@@ -320,6 +398,13 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
     }
   }
 
+  /**
+   * Method that adds random moves whenever the given move sequence of an individual is not already too long.
+   * Every sequence being already two times the size of the initial individual is considered too long.
+   * Whether a given move will be added or not is random with a chance not greater than 40%.
+   * 
+   * @param individual {@code MovesArrayIndividual} that is going to be mutated.
+   */
   private void mutateAddMoves(final MovesArrayIndividual individual) {
     for (int m = 0; m < individual.object.length; m++) {
       Moves moves = individual.object[m];
@@ -336,6 +421,13 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
     }
   }
 
+  /**
+   * Method that removes random moves whenever the given move sequence of an individual is not already too short.
+   * Every sequence being only 0.8 times the size of the initial individual is considered too short.
+   * Whether a given move will be removed or not is random with a chance not greater than 10%.
+   * 
+   * @param individual {@code MovesArrayIndividual} that is going to be mutated.
+   */
   private void mutateRemoveMoves(final MovesArrayIndividual individual) {
     for (int m = 0; m < individual.object.length; m++) {
       Moves moves = individual.object[m];
@@ -350,12 +442,29 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
     }
   }
 
+  /**
+   * Method to dynamically adjust the mutation rate based on the position in the moves sequence, the size of the 
+   * very same as well as the maximum allowed mutation rate.
+   * Towards the end of a move sequence the likelihood for a mutation rises. This makes sense in two ways:
+   *  1.  it is advisable to change the later moves rather than the earlier ones, since only there improvements can be
+   *      achieved (by finding holes the heuristic omitted)
+   *  2.  by having a variable mutation rate that increases, it is more likeli that a mutation takes place
+   * 
+   * @param i {@code int} being the position in the array of moves
+   * @param length {@code int} being the length of the moves array (total number of all moves)
+   * @param max {@code double} being the maximum allowed mutation rate.
+   * @return [description]
+   */
   protected double mutationRate(int i, int length, double max) {
     return max * Math.pow((double) i / length, 4);
   }
 
+  /**
+   * Method that selects the best individuals in the population of the current iteration. The number of best individuals
+   * to be selected is specified in {@code NUMBER_OF_SELECTION}.
+   */
   protected void select() {
-    // Select the four best population
+    // Select the best individuals of the population
     parents.clear();
     int j = 0;
     for (MovesArrayIndividual individual : population) {
@@ -369,19 +478,32 @@ public class GeneticAlgorithm extends AbstractAlgorithm<Moves[]> {
     population.addAll(parents);
   }
 
+  /**
+   * Method that evaluates the solutions generated by the genetic algorithm. First ensures that they represent valid
+   * solutions at all (no placement outside the field or in the middle of the air).
+   * Afterwards the score is calculated to make the generated solution comparable to the others.
+   * 
+   * @param individual {@code MovesArrayIndividual} to be evaluated
+   * @throws TimeoutException whenever the time is insufficient to complete the evaluation
+   */
   protected void evaluate(final MovesArrayIndividual individual) throws TimeoutException {
+    //  Check if sufficient time remains to evaluate the individual.
     if (state.getTimer().remainingTimeMillis() < BREAK_MS)
       throw new TimeoutException("Timeout");
 
+    //  Obtain the field needed for evaluation.
     final Field field = state.getMyField();
 
+    //  Check whether given move sequence is valid and add the shape to the field for evaluation.
     for (Moves moves : individual.object) {
       moves.checkAndApply();
       field.addShape(moves.shape);
     }
 
+    //  Calculate the score of the solution with the given evaluation function.
     individual.score = evaluationFunction.evaluate(state);
 
+    //  Remove the shapes again to allow further evaluation in the future.
     for (Moves moves : individual.object) {
       field.removeShape(moves.shape);
       moves.reset();
